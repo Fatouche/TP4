792b498fb6405ec7c2ff72eea0cff0caf41c8e4d
fatouche <foucaud.giovanny@gmail.com>
2017-03-07 15:59:01 +0100

    Fin cours

--- src/PolynomeRegulier.cpp
+++ src/PolynomeRegulier.cpp
@@ -1,9 +1,9 @@
 #include "PolynomeRegulier.hpp"
 
 PolynomeRegulier(const Couleur & couleur, const Point & centre, int rayon, int nbCotes) :
-  FigureGeometrique(couleur), _nbPoints(nbCotes)
+  FigureGeometrique(couleur), _nbPoints(nbCotes+1)
 {
-  _points = new Point(nbCotes);
+  _points = new Point(nbCotes+1);
 }
 
 void PolynomeRegulier::afficher() const{
